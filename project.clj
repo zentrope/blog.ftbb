b56c61590fc3c23d0c32ad57c0d6d020201e91e9
Keith Irwin <keith@zentrope.com>
2018-04-29 17:10:36 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,12 +2,11 @@
   :description "A static blog site maker-upper."
   :url "https://github.com/zentrope/blog.ftbb"
   :license {:name "EPL" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [hiccup "1.0.5"]]
+  :dependencies [[org.clojure/clojure "1.10.0-alpha4"]
+                 [org.clojure/data.xml "0.2.0-alpha5"]
+                 [hiccup "2.0.0-alpha1"]]
   :clean-targets ["target" "pub"]
-  :jvm-opts ["-Djava.awt.headless=true"]
-  :min-lein-version "2.5.1"
+  :min-lein-version "2.8.1"
   :profiles {:uberjar {:aot :all}}
-  :plugins [[lein-ancient "0.6.7"]]
+  :plugins [[lein-ancient "0.6.15"]]
   :main ^:skip-aot blog.main)
